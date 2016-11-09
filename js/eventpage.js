ca6cdecd1d18bab4392c047661d0a5c2faebc615
Walter Bender <walter.bender@gmail.com>
2016-11-09 12:50:24 -0500

    add event

--- js/eventpage.js
+++ js/eventpage.js
@@ -2,6 +2,14 @@
 
 const FUTUREEVENTSLIST = [
     {
+	'title': "Nature’s Inventions and Industrial Innovations",
+        'link': 'http://MOTSummit.com',
+        'image': 'shell.jpg',
+	'date': 'Thursday, November 10, 2016 / 7:00 PM – 8:00 PM',
+	'description': '<b>An Introduction to the Bionic Principles and Methods of Biomimicry</b><br>As a preview of the Masters of Design conference curated by IAM Foundation, which is part of the Masters of Tomorrow Summit (MOTSummit.com), taking place on November 30th in Wynwood, industrial designer Franco Lodato will give an insightful presentation on his experience around nature-inspired Industrial Design.<br><br>Venture Cafe (CIC Miami, 1951 NW 7th Ave, Miami, FL, 33136)'
+    },
+
+    {
 	'title': 'Masters of Tomorrow',
         'link': 'https://www.facebook.com/MOTSummit/',
         'image': 'mot.png',
